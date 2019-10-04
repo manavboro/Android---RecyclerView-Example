2ec7e442a41abb6ea51d4d3e6ea31b7c33f64b38
Manab Boro <manab@dailyapps.us>
2019-10-04 23:43:09 +0530

    Horizontal activity

---
+++ app/src/main/java/com/manav/boro/apps/recyclerviewforgithub/activities/HorizontalRecyclerView.java
@@ -0,0 +1,16 @@
+package com.manav.boro.apps.recyclerviewforgithub.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.manav.boro.apps.recyclerviewforgithub.R;
+
+public class HorizontalRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horizontal_recycler_view);
+    }
+}
